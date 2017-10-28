6b336a82aebe5d56574a56aca5fe7abd33005075
jefryhdz <jefryhdz@gmail.com>
2017-10-28 07:51:49 -0600

    haciendo el calendario

--- Lab3_P3_JefryHernandez.cpp
+++ Lab3_P3_JefryHernandez.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib>
 
 using namespace std;
 
 int menu();
-int*** ruffini();
+void ruffini();
 int*** crearMatriz(int);
 int*** llenar(int***,int,int*);
 int** mediana();
@@ -12,6 +13,7 @@ bool fechavalida(string);
 void print(int***,int);
 int* exponentes(int);
 int*** calcular(int***,int,int);
+void calculardia(string);
 
 int main(){
 	int resp=4;
@@ -30,10 +32,13 @@ int main(){
 					string ffecha;
 					cout<<"Ingrese la fecha en string "<<endl;
 					cin>>ffecha;
-					fechas.push_back(ffecha);
-					//fecha(ffecha);
+					bool date = fechavalida(ffecha);
+					if(date){
+						calculardia(ffecha);
+						fechas.push_back(ffecha);
+					}
 					cout<<"Desea ingresar una fecha [s/n]"<<endl;
-					cin>>resp;					
+					cin>>res;					
 				}while(res=='s');
 				break;
 		}	
@@ -51,7 +56,7 @@ int menu(){
 	cin>>resp;
 	return resp;
 }
-int*** ruffini(){
+void ruffini(){
 	int*** matriz;
 	int* exp;
 	int size;
@@ -87,6 +92,47 @@ int*** crearMatriz(int size){
 	return matriz;
 }
 bool fechavalida(string fecha){
+	string ano = fecha.substr(0,4);
+	string mes = fecha.substr(4,2);
+	string dia = fecha.substr(6,8);
+	int year= atoi(ano.c_str());
+	int month = atoi(mes.c_str());
+	int day= atoi(dia.c_str());
+	cout<<year<<" "<<month<<" "<<day<<"sd"<<endl;
+	if(month==1||month==3||month==5||month==7||month==8||month==10||month==12){
+		if(day>0&&day<32){
+			return true;
+		}			
+	}if(month==4||month==6||month==9||month==11){
+		if(day>0&&day<31){		
+			return true;
+		}else{
+			cout<<"fecha no valida"<<endl;
+			return false;
+		}
+	}if(year%4==0){
+		if(month==2){
+			if(day>0&&day<30){			
+				return true;
+			}else{
+				cout<<"fecha no valida"<<endl;
+				return false;
+			}
+		}	
+	}if(year%4!=0&&year%100!=0){
+		if(month==2){
+			if(day>0&&day<29){
+				return true;
+			}else{
+				cout<<"fecha no valida"<<endl;
+				return false;
+			}			
+		}
+	}
+	else{
+		cout<<"fecha no valida"<<endl;
+		return false;
+	}
 
 
 }int*** llenar(int*** matriz,int size,int* coeficientes){
@@ -157,3 +203,67 @@ int*** calcular(int*** matriz,int size,int a){
         }
 	return matriz;
 }
+void calculardia(string fecha){
+	string weekday;
+	string ano = fecha.substr(0,4);
+	string mes = fecha.substr(4,2);
+	string dia = fecha.substr(6,8);
+	cout<<"w"<<endl;
+	int year= atoi(ano.c_str());
+	int month = atoi(mes.c_str());
+	int day= atoi(dia.c_str());
+	int a=0,b=0,c=0,d=0,e=0;
+	if(year>=1700&&year<1800){
+		a=5;
+	}if(year>=1800&&year<1900){
+		a=3;	
+	}if(year>=1900&&year<2000){
+		a=1;
+	}if(year>=2000&&year<2100){
+		a=0;
+	}if(year>=2100&&year<2200){
+		a=-2;
+	}if(year>=2200&&year<2300){
+		a=-4;
+	}
+	string digitos=ano.substr(2,4);//digitos porque ocupo los dos ultimos digitos del año
+	int digits = atoi(digitos.c_str());	
+	cout<<digits<<endl;
+	b=digits+(digits/4);
+	if(year%4==0&&year%100!=0){
+		c=-1;	
+	}if(month==1||month==10){
+		d=6;
+	}if(month==2||month==3||month==11){
+		d=2;	
+	}if(month==4||month==7){
+		d=5;
+	}if(month==6){
+		d=3;
+	}if(month==8){
+		d=1;
+	}if(month==9||month==12){
+		d=4;
+	}
+	e=day;
+	int total=a+b+c+d+e;
+	total-=7;
+	cout<<total<<endl;
+	if(total ==1){
+		weekday="Lunes";
+	}if(total==2){
+		weekday="Martes";	
+	}if(total==3){
+		weekday="Miercoles";
+	}if(total==4){
+		weekday="Jueves";
+	}if(total==5){
+		weekday="Viernes";
+	}if(total==6){
+		weekday="Sabado";
+	}if(total==0){
+		weekday="Domingo";
+	}
+	cout<<weekday<<endl;
+}
+	
